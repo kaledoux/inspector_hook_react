83f4fdb169fad392159d000ff74e42aa66ac3ef2
Kyle <kaledoux@gmail.com>
2021-06-03 08:52:25 -0400

    app creation of basic componenets

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+	const Title = () => {
+		return (
+			<div>
+				<h1>Inspector Hook</h1>
+				<p>Create a new bin to collect api requests, or view the requests of a current bin</p>
+			</div>
+		);
+	};
+
+	const NewBin = () => {
+		return (
+			<div>
+				<h2>Create a new inspector bin:</h2>
+				<button>Create Bin</button>
+			</div>
+		);
+	};
+
 	return (
 		<div className="App">
-			<h1>Inspector Hook</h1>
-			<p>Create a new bin to collect api requests, or view the requests of a current bin</p>
-			<h2>Create a new bin:</h2>
-			<button>Create Bin</button>
+			<Title />
+			<NewBin />
 		</div>
 	);
 }
