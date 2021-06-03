9063afbd22be76dcd99fabcf622f70e7698bc9df
Kyle <kaledoux@gmail.com>
2021-06-03 08:40:00 -0400

    basic landing page outline

--- src/App.js
+++ src/App.js
@@ -2,24 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Inspector Hook</h1>
+			<p>Create a new bin to collect api requests, or view the requests of a current bin</p>
+			<h2>Create a new bin:</h2>
+			<button>Create Bin</button>
+		</div>
+	);
 }
 
 export default App;
